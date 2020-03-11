608e9684f970baeec14eabd06e6054935dd06df7
hend <henddiab132@gmail.com>
2020-03-12 01:34:52 +0200

    final version

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -9,28 +9,29 @@ import { ServiceService } from "./../service.service"
   styleUrls: ["./add-contact.component.css"]
 })
 export class AddContactComponent implements OnInit {
-  addContact: FormGroup;
-  fileData: File = null;
-  userData;
-  checker: boolean = true;
+  addContact: FormGroup; //form
+  fileData: File = null; 
+  userData; // all contacts data
+  checker: boolean = true;  // to check on phone redunduncy
   imagee: any;
-  imageSrc: any = "https://www.gateshead.church/dowo/wp-content/uploads/2019/09/person-icon.png";
-  arr = [];
+  imageSrc: any = "https://www.gateshead.church/dowo/wp-content/uploads/2019/09/person-icon.png"; // default image
 
 
 
   constructor(private service: ServiceService, private router: Router) { }
 
   readURL(event: any) {
-    this.fileData = <File>event.target.files[0];
+    this.fileData = <File>event.target.files[0]; // to get type of file and check on  it, then read value of image
     this.preview();
   }
   preview() {
+    //check that files shown are only images
     var mimeType = this.fileData.type;
     if (mimeType.match(/image\/*/) == null) {
       return;
     }
 
+    // read value of selected image
     var reader = new FileReader();
     reader.readAsDataURL(this.fileData);
     reader.onload = _event => {
@@ -41,6 +42,8 @@ export class AddContactComponent implements OnInit {
       });
     };
   }
+
+  // function loop on all data and check if phone is already found
   check(form: FormGroup) {
 
     if (this.userData.length > 0) {
@@ -66,6 +69,8 @@ export class AddContactComponent implements OnInit {
       mobileNumber: new FormControl(null, [Validators.required, Validators.pattern(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)])
     });
 
+
+    //get all data
     this.service.getData().subscribe(data => {
       this.userData = data;
     });
@@ -73,18 +78,17 @@ export class AddContactComponent implements OnInit {
 
 
   onSubmit() {
-    var newContact = this.addContact;
+
     var headers = { 'Content-Type': 'application/json' };
-    if (this.check(newContact)) {
-      this.service.postData(newContact.value, { headers }).subscribe(response => {
-        console.log(response);
-        this.service.nextfun(response);
+    if (this.check(this.addContact)) {
+      this.service.postData(this.addContact.value, { headers }).subscribe(response => {
+        this.service.nextfun(response); // get updates
       });
       this.router.navigate(["/contacts"]);
     }
 
     else {
-      alert('phoneNumber Found, enter another one')
+      alert('phoneNumber Found, enter another one');
     }
   }
 }
--- src/app/phone-book/phone-book.component.ts
+++ src/app/phone-book/phone-book.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ServiceService } from "./../service.service"
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,6 +10,8 @@ import { Router } from '@angular/router';
   styleUrls: ["./phone-book.component.css"]
 })
 export class PhoneBookComponent implements OnInit {
+
+  addContact: FormGroup;
   contacts: any = [];
   fileData: File = null;
   imagee: any;
@@ -30,35 +33,37 @@ export class PhoneBookComponent implements OnInit {
     reader.readAsDataURL(this.fileData);
     reader.onload = _event => {
       this.user.image = reader.result;
-      console.log(this.user);
-
       this.imagee = reader.result;
+      this.addContact.patchValue({
+        image: this.imagee
+      });
     };
   }
 
 
+  // get data of contact to be editted
   edit(event) {
     this.service.getSigleData(event.target.id).subscribe(data => {
       this.user = data;
     })
   }
 
-  onSubmit(img, fName, lName, phone) {
-    
-    this.user.firstName = fName.value;
-    this.user.lastName = lName.value;
-    this.user.image = img.value;
-    this.user.mobileNumber = phone.value;
 
-    this.service.editContact(this.user.id, this.user).subscribe(response => {
+  onSubmit() {
+
+    this.service.editContact(this.user.id, this.addContact.value).subscribe(response => {
       this.service.nextfun(response);
 
+
+      //get data again after editting
       this.service.getData().subscribe(data => {
         this.contacts = data;
       })
     });
   }
 
+
+  // delete contact
   delete(event, contact) {
     console.log(event.target.id);
     contact.remove();
@@ -67,13 +72,20 @@ export class PhoneBookComponent implements OnInit {
 
   ngOnInit() {
 
+    this.addContact = new FormGroup({
+      image: new FormControl('', Validators.required),
+      firstName: new FormControl('', [Validators.required, Validators.pattern(/^[a-z]{4,}$/)]),
+      lastName: new FormControl('', [Validators.required, Validators.pattern(/^[a-z]{4,}$/)]),
+      mobileNumber: new FormControl('', [Validators.required, Validators.pattern(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)])
+    });
+
     this.service.getData().subscribe(data => {
       this.contacts = data;
     });
 
+    // push new updates
     this.service.BehaviorSubjectData.subscribe(items => {
-
-      this.contacts.push(items)
+      this.contacts.push(items);
     })
   }
 }
